c701ce6dd53d53c8638b05e1ae2c1ee024edab1f
cezanekarki <karkicezane123@gmail.com>
2025-11-21 08:31:55 -0600

    Data refining, cleaning and feature engineering

--- refining_data.py
+++ refining_data.py
@@ -4,58 +4,37 @@ import pandas as pd
 
 def data_refiner(data, data_saving_path):
     columns_to_drop = [
-        'Unnamed: 5',
-        'Unnamed: 5.1',
-        
-        'Unnamed: 6',
-        'Unnamed: 7',
-        'Unnamed: 8',
-        'Unnamed: 6.1',
-        'Unnamed: 7.1',
-        'Unnamed: 8.1',
-        'Cooling load (GJ)',
-        'Heating load (GJ)',
-        'Hot water load (GJ)',
-        'Cooling load (GJ).1',
-        'Heating load (GJ).1',
-        'Hot water load (GJ).1',
+        'Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8', 
+        'Unnamed: 5.1', 'Unnamed: 6.1', 'Unnamed: 7.1', 'Unnamed: 8.1',
+        'Cooling load (GJ)', 'Heating load (GJ)', 'Hot water load (GJ)',
+        'Cooling load (GJ).1', 'Heating load (GJ).1', 'Hot water load (GJ).1',
+        'Boiler (m3)', 'Fuel cell (m3)', 'Gas engine (m3)',
+        'Absorption chiller 1 (m3)', 'Absorption chiller 2 (m3)', 'Absorption chiller 3 (m3)'
     ]
 
-
     renaming_map = {
-        'Solar energy generation (kW)': 'Solar_Generation_kW',
-        'Electricity imported from grid (kW)': 'Electricity_Imported_kW',
-        'Gas engine (kW)': 'Gas_Engine_kW',
-        'Fuel cell (kW)': 'Fuel_Cell_kW',
-        
-        'Electricity load (kW)': 'Electric_Load_kW_A',      
-        'Cooling load (kW)': 'Cooling_Load_kW_A',
-        'Heating load (kW)': 'Heating_Load_kW_A',
-        'Hot water load (kW)': 'Hot_Water_Load_kW_A',
-        
-        'Electricity load (kW).1': 'Electric_Load_kW_B', 
-        'Cooling load (kW).1': 'Cooling_Load_kW_B',
-        'Heating load (kW).1': 'Heating_Load_kW_B',
-        'Hot water load (kW).1': 'Hot_Water_Load_kW_B',
-        
-        'Gas 1(GJ/h)': 'Gas_1_GJh',
-        'Gas 2(GJ/h)': 'Gas_2_GJh',
-        'Fuel cell 1(GJ/h)': 'Fuel_Cell_1_GJh',
-        'Fuel cell 2(GJ/h)': 'Fuel_Cell_2_GJh',
-        'Fuel cell 3(GJ/h)': 'Fuel_Cell_3_GJh',
-        
-        'Boiler (m3)': 'Boiler_Meter_m3',
-        'Fuel cell (m3)': 'Fuel_Cell_Meter_m3',
-        'Gas engine (m3)': 'Gas_Engine_Meter_m3',
-        'Absorption chiller 1 (m3)': 'Absorption_Chiller_1_m3',
-        'Absorption chiller 2 (m3)': 'Absorption_Chiller_2_m3',
-        'Absorption chiller 3 (m3)': 'Absorption_Chiller_3_m3',
-
+        'Solar energy generation (kW)': 'PV_Generation_kW',
+        'Electricity imported from grid (kW)': 'Grid_Import_kW',
+        'Gas engine (kW)': 'GasEngine_Generation_kW',
+        'Fuel cell (kW)': 'FuelCell_Generation_kW',        
+        'Electricity load (kW)': 'Electric_Load_A_kW',      
+        'Cooling load (kW)': 'Cooling_Load_A_kW',
+        'Heating load (kW)': 'Heating_Load_A_kW',
+        'Hot water load (kW)': 'HotWater_Load_A_kW',
+        'Electricity load (kW).1': 'Electric_Load_B_kW', 
+        'Cooling load (kW).1': 'Cooling_Load_B_kW',
+        'Heating load (kW).1': 'Heating_Load_B_kW',
+        'Hot water load (kW).1': 'HotWater_Load_B_kW',
         'Horizontal solar irradition (W)': 'Solar_Irradiation_W',
         'Outdoor air temperature (℃)': 'Outdoor_Air_Temp_C',
         'Outdoor air humidity (%)': 'Outdoor_Air_Humidity_percent',
         'Wind speed (m/s)': 'Wind_Speed_ms',
         'Wind direction': 'Wind_Direction_deg',
+        'Gas 1(GJ/h)': 'Gas1_Input_GJh',
+        'Gas 2(GJ/h)': 'Gas2_Input_GJh',
+        'Fuel cell 1(GJ/h)': 'FuelCell1_Input_GJh',
+        'Fuel cell 2(GJ/h)': 'FuelCell2_Input_GJh',
+        'Fuel cell 3(GJ/h)': 'FuelCell3_Input_GJh'
     }
 
 
